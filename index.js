d43aa81dff0b1db11d95c8b046f88da373f5913a
miker83z <mikerz83@gmail.com>
2021-05-26 13:21:17 +0200

    cors

--- index.js
+++ index.js
@@ -2,23 +2,34 @@
 
 var path = require('path');
 var http = require('http');
+var cors = require('cors');
 
 var oas3Tools = require('oas3-tools');
 var serverPort = 8080;
 
 // swaggerRouter configuration
 var options = {
-    routing: {
-        controllers: path.join(__dirname, './controllers')
-    },
+  routing: {
+    controllers: path.join(__dirname, './controllers'),
+  },
 };
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+var expressAppConfig = oas3Tools.expressAppConfig(
+  path.join(__dirname, 'api/openapi.yaml'),
+  options
+);
 var app = expressAppConfig.getApp();
+app.use(cors());
 
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+  console.log(
+    'Your server is listening on port %d (http://localhost:%d)',
+    serverPort,
+    serverPort
+  );
+  console.log(
+    'Swagger-ui is available on http://localhost:%d/docs',
+    serverPort
+  );
 });
-
